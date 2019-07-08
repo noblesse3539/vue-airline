e51611af0a21647acf0d3e1e0748d2ab5cb81cbd
opalcat <noblesse3539@daum.net>
2019-07-08 15:49:26 +0900

    opalcat|Req 2-14_1|AirLine 리스트 작성중

---
+++ bision_project/src/components/airlinedetail/AirLine.vue
@@ -0,0 +1,21 @@
+<template>
+    <a v-bind:href="'#'">
+        <v-card>
+            <v-img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAD1BMVEUAW7v/1QAAVcF7l1n/2QAC/jD2AAAAzUlEQVR4nO3QsQGAMAzAsBT4/2b2eO0onaAZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACASx62ednmY5vD5qSclJNyUk7KSTkpJ+WknJSTclJOykk5KSflpJyUk3JSTspJOSkn5aSclJNyUk7KSTkpJ+WknJSTclJOykk5KSflpJyUk3JSTspJOSkn5aSclJNyUk7KSTkpJ+WknJSTclJOykk5KSflpJyUk3JSTspJOSkn5aSclJNyUk7KSTkpJ+WknJSTclJOykk5KSflpH7zk2pa0LCuDAAAAABJRU5ErkJggg=="></v-img>
+            <v-card-title>우크라이나!!!</v-card-title>
+        </v-card>
+    </a>
+</template>
+<script>
+export default {
+    name: 'AirLine',
+    components: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
---
+++ bision_project/src/views/AirLineDetailPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <AirLineList></AirLineList>
+</template>
+<script>
+import AirLineList from '../components/airlinedetail/AirLineList'
+export default {
+    name:'AirLineDetailPage',
+    components: {
+        AirLineList,
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
