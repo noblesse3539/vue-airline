49de1c7e10c6ae53f0ca54d3d464d745e1afd9e4
sshyeri <sshyeri.k@gmail.com>
2019-07-12 16:29:12 +0900

    Hyeri|Req.2-15-2|page1-나라, 도시 외 입력 폼 완성

--- bision_project/src/components/PortfolioWrite.vue
+++ bision_project/src/components/PortfolioWrite.vue
@@ -1,20 +1,79 @@
 <template>
-  <v-dialog v-model="dialog">
+  <v-dialog persistent v-model="dialog" max-width="800px">
     <v-card>
       <v-card-title>
-        <v-btn color="black" flat icon @click="close">
+        <v-btn color="black" flat icon @click="closePW">
           <v-icon>close</v-icon></v-btn>
         <span class="headline">New Portfolio</span>
         <v-spacer></v-spacer>
-        <v-btn flat icon color="blue" @click="save">
+        <v-btn flat icon color="blue" @click="savePW">
         <v-icon large >check</v-icon></v-btn>
       </v-card-title>
 
       <v-card-text>
         <v-container grid-list-md>
+          <v-layout wrap>
+            <!-- 시작날짜 -->
+            <v-flex xs12 sm6>
+              <v-menu ref="fromMenu" v-model="fromMenu" :close-on-content-click="false"
+                      :nudge-right="40" :return-value.sync="portfolio.fromDate" lazy transition="scale-transition"
+                      min-width="290px" offset-y full-width>
+                <template v-slot:activator="{ on }">
+                  <v-text-field v-model="portfolio.fromDate" label="시작 날짜*" prepend-icon="event" readonly v-on="on"></v-text-field>
+                </template>
+                <v-date-picker :max="toDate" v-model="portfolio.fromDate" no-title scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="blue" @click="fromMenu = false">Cancel</v-btn>
+                  <v-btn flat color="blue" @click="$refs.fromMenu.save(portfolio.fromDate)">OK</v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-flex>
+            <!-- 종료날짜 -->
+            <v-flex xs12 sm6>
+              <v-menu ref="toMenu" v-model="toMenu" :close-on-content-click="false"
+                      :nudge-right="40" :return-value.sync="portfolio.toDate" lazy transition="scale-transition"
+                      min-width="290px" offset-y full-width>
+                <template v-slot:activator="{ on }">
+                  <v-text-field v-model="portfolio.toDate" label="종료 날짜*" prepend-icon="event" readonly v-on="on"></v-text-field>
+                </template>
+                <v-date-picker :min="portfolio.fromDate" v-model="portfolio.toDate" no-title scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="blue" @click="toMenu = false">Cancel</v-btn>
+                  <v-btn flat color="blue" @click="$refs.toMenu.save(portfolio.toDate)">OK</v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-flex>
 
+            <!-- 소요시간 -->
+            <v-flex xs6>
+              <v-text-field prepend-icon="schedule" v-model="portfolio.duration" label="소요시간*" clearable
+                            clear-icon="clear" hint="ex) 10시간, 1박2일" persistent-hint></v-text-field>
+            </v-flex>
+
+            <!-- 통화 변환 구현필요 -->
+            <v-flex xs6>
+              <v-text-field prepend-icon="fa-wallet" label="가격*" prefix="₩" v-model.number="portfolio.cost"  clearable
+                            clear-icon="clear" type="number"></v-text-field>
+            </v-flex>
+
+            <!-- 인원 -->
+            <v-flex xs6>
+              <v-text-field prepend-icon="far fa-user" suffix="명" v-model.number="portfolio.minTrav" label="최소 인원*"
+                            clearable clear-icon="clear" type="number"></v-text-field>
+            </v-flex>
+            <v-flex xs6>
+              <v-text-field prepend-icon="fa-users" suffix="명" v-model.number="portfolio.maxTrav" label="최대 인원*"
+                            clearable clear-icon="clear" type="number"></v-text-field>
+            </v-flex>
+
+            <!-- description 글자수 제약?-->
+            <v-flex xs12>
+              <v-textarea label="상품 요약*" auto-grow solo v-model="portfolio.desc"></v-textarea>
+            </v-flex>
+          </v-layout>
         </v-container>
       </v-card-text>
+      <small>*은 필수항목입니다.</small>
     </v-card>
   </v-dialog>
 </template>
@@ -23,14 +82,25 @@
 export default {
   data (){
     return{
-      dialog: true
+      dialog: true,
+      fromMenu: false,
+      toMenu: false,
+      portfolio:{
+        fromDate:'',
+        toDate:'',
+        duration: '',
+        cost:'',
+        minTrav:'',
+        maxTrav: '',
+        desc:'',
+      }
     }
   },
   methods : {
-    save(){
+    savePW(){
       this.$emit('close')
     },
-    close(){
+    closePW(){
       this.$emit('close', false);
     }
   }
