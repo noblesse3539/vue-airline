cfb538379711cf59536efd94bd44ffbaafc08606
류정관 <starlight747@naver.com>
2019-07-19 12:12:04 +0900

    류정관|Req 2-X|가이드 및 가이드서비스 Create Update 코드 수정

--- backend/routes/api/guideservice/controller.js
+++ backend/routes/api/guideservice/controller.js
@@ -13,52 +13,36 @@ exports.deleteGuideService = (req,res) =>{
           message: error.message
       })
   }
-  GuideService.deleteByTitle(req.params.guideID,req.params.title)
+  GuideService.deleteByTitle(req.params,req.params.title)
   .then(respond)
   .then(onError)
 }
 
-exports.updateGuideService = (req,res) =>{
-  // console.log(req)
-  let {
-    title,
-    nation,
-    city,
-    fromDate,
-    toDate,
-    duration,
-    coreImg,
-    imgList,
-    cost,
-    desc,
-    detail,
-    numOfGuest,
-    reviewList}=req.body;
-  const respond=()=>{
-    res.json({
-      message:"update GuideService OK"
-    })
+exports.updateGuideService = (req, res) => {
+    GuideService.update=(_id)=>{(
+        {_id: _id},
+        { $set: req.body },
+        (err, output) => {
+            if(err) res.status(500).json({ error: 'database failure' })
+            if(!output.n) return res.status(404).json({ error: 'user not found'})
+            res.json({ message: 'user updated'})
+        }
+    )
   }
 
-  const onError = (error) => {
-      res.status(403).json({
-          message: error.message
-      })
-  }
-  GuideService.updateByTitle(req.params.guideID,req.params.title,title,nation,city,fromDate,toDate,duration,coreImg,imgList,cost,desc,detail,numOfGuest,reviewList)
-  .then(respond)
-  .then(onError)
+    GuideService.findGSByUserObIdTitle(req.params.userObId,req.params.title)
+    .then(GuideService.update(res.data._id))
 }
 
-exports.findGSByGuideIdTitle = (req,res)=>{
 
+exports.findGSByUserObIdTitle = (req,res)=>{
   const onError = (error) => {
       res.status(403).json({
           message: error.message
       })
   }
 
-  GuideService.findGSByGuideIdTitle(req.params.guideID,req.params.title)
+  GuideService.findGSByUserObIdTitle(req.params.userObId,req.params.title)
   .then(
       guideservice => {
           res.json(guideservice)
@@ -69,60 +53,11 @@ exports.findGSByGuideIdTitle = (req,res)=>{
 
 
 exports.createGuideService = (req,res) =>{
-  console.log(req)
-  console.log(req.body.nation)
-  const {
-    guideID,
-    title,
-    nation,
-    city,
-    fromDate,
-    toDate,
-    duration,
-    coreImg,
-    imgList,
-    cost,
-    desc,
-    detail,
-    numOfGuest,
-    reviewList} = req.body
-  let newGuideService=null
+  console.log(req.body);
+  var GS=new GuideService(req.body);
 
-  // const createGS = (guideservice) =>{
-  //   if(guideservice){
-  //     throw new Error('guideservice exists')
-  //   } else{
-  //     return GuideService.create(nation)
-  //   }
-  // }
-
-  const repond=()=>{
-    res.json({
-      message:"create OK"
-    })
-  }
-
-  const onError = (error) => {
-      res.status(403).json({
-          message: error.message
-      })
-  }
-
-  GuideService.create(
-    guideID,
-    title,
-    nation,
-    city,
-    fromDate,
-    toDate,
-    duration,
-    coreImg,
-    imgList,
-    cost,
-    desc,
-    detail,
-    numOfGuest,
-    reviewList)
-  .then(repond)
-  .catch(onError)
+  GS.save(err => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(GS);
+  })
 }
