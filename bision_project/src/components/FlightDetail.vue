38c826949327ffa9234ca4c8af5a2b64e5977278
sshyeri <sshyeri.k@gmail.com>
2019-07-29 14:33:17 +0900

    Hyeri|Req.3-5-6|FlightDetail_예약하기

--- bision_project/src/components/FlightDetail.vue
+++ bision_project/src/components/FlightDetail.vue
@@ -24,8 +24,8 @@
                 <h2>{{flight.InArrivalTime}}</h2>
                 <p>{{flight.DestinationAirportCode}}</p>
               </div>
-              <v-icon v-if="isInVisible" color="#35c235" large>fa-chevron-up</v-icon>
-              <v-icon v-if="!isInVisible" :color="`${inArrow? '#35c235':'grey'}`" large>fa-chevron-down</v-icon>
+              <v-icon class="arrow" v-if="isInVisible" color="#35c235" large>fa-chevron-up</v-icon>
+              <v-icon class="arrow" v-if="!isInVisible" :color="`${inArrow? '#35c235':'grey'}`" large>fa-chevron-down</v-icon>
             </div>
             <div v-if="isInVisible" class="moreDetail">
               <div>
@@ -62,8 +62,8 @@
                 <h2>{{flight.OutArrivalTime}}</h2>
                 <p>{{flight.OriginAirportCode}}</p>
               </div>
-              <v-icon v-if="isOutVisible" color="#35c235" large>fa-chevron-up</v-icon>
-              <v-icon v-if="!isOutVisible" :color="`${outArrow? '#35c235':'grey'}`" large>fa-chevron-down</v-icon>
+              <v-icon class="arrow" v-if="isOutVisible" color="#35c235" large>fa-chevron-up</v-icon>
+              <v-icon class="arrow" v-if="!isOutVisible" :color="`${outArrow? '#35c235':'grey'}`" large>fa-chevron-down</v-icon>
             </div>
             <div v-if="isOutVisible" class="moreDetail">
               <div>
@@ -88,44 +88,46 @@
           <div class="ad">
            <div class="costInfo">
               <h2>최저가로 예약하기</h2>
-                <a class="book" :href="flight.LowestDeeplinkUrl" target="_blank">
+              <div class="book">
+                <div class="overlay"></div>
+                <a class="bookLink" :href="flight.LowestDeeplinkUrl" target="_blank">
                   <img width="100px" :src="flight.LowestAgentsImageUrl" alt="">
-                  <h1>{{flight.CurrencySymbol}}{{flight.LowestPrice}}</h1>
+                  <!-- <h2>{{flight.CurrencySymbol}}{{flight.LowestPrice}} <v-icon style="margin-left:1rem;"large color="green">fa-arrow-right</v-icon></h2> -->
+                  <h2>{{flight.CurrencySymbol}}{{flight.LowestPrice}}</h2>
                 </a>
-                <div class="readBeforeBooking" @click="isRBBVisible = !isRBBVisible">
-                  <h5>예약전에 확인해주세요.</h5>
-                  <v-icon v-if="isRBBVisible" color="#35c235" small>fa-chevron-up</v-icon>
-                  <v-icon v-if="!isRBBVisible" color="#35c235" small>fa-chevron-down</v-icon>
-                </div>
-                <div class="readBeforeBooking__Toggle" v-show="isRBBVisible">
-                  <p>표시된 요금에는 모든 세금 및 수수료가 포함되어 있으나, 예약 전에 웹사이트에서
-                    <b>티켓의 세부정보, 최종 가격, 약관</b>을 반드시 확인하시기
-                    바랍니다.
-                  </p>
-                  <ul>
-                    <li>
-                      <b>☞ 추가 요금 확인</b>
-                      <p style="margin:.75em;">일부 항공사/여행사의 경우 <b>수하물, 보험</b> 또는 <b>신용카드</b>
-                        사용에 대한 추가 요금을 부과합니다.</p>
-                      </li>
-                      <li>
-                        <b>☞ 12~16세 승객에 대한 이용 약관을 확인하십시오.</b>
-                        <p style="margin:.75em;">보호자를 동반하지 않고 혼자 여행하는 어린 승객에게는
-                          제한 사항이 적용될 수 있습니다.</p>
-                        </li>
-                      </ul>
-                    </div>
-                    <div>
               </div>
-            </div>
-            <div class="recommendProducts">
-              가이드 상품 추천 영역
-            </div>
-
+              <div class="readBeforeBooking" @click="isRBBVisible = !isRBBVisible">
+                <h5>예약전에 확인해주세요.</h5>
+                <v-icon v-if="isRBBVisible" color="#35c235" small>fa-chevron-up</v-icon>
+                <v-icon v-if="!isRBBVisible" color="#35c235" small>fa-chevron-down</v-icon>
+              </div>
+              <div class="readBeforeBooking__Toggle" v-show="isRBBVisible">
+                <p>표시된 요금에는 모든 세금 및 수수료가 포함되어 있으나, 예약 전에 웹사이트에서
+                  <b>티켓의 세부정보, 최종 가격, 약관</b>을 반드시 확인하시기
+                  바랍니다.
+                </p>
+                <ul>
+                  <li>
+                    <b>☞ 추가 요금 확인</b>
+                    <p style="margin:.75em;">일부 항공사/여행사의 경우 <b>수하물, 보험</b> 또는 <b>신용카드</b>
+                      사용에 대한 추가 요금을 부과합니다.</p>
+                    </li>
+                    <li>
+                      <b>☞ 12~16세 승객에 대한 이용 약관을 확인하십시오.</b>
+                      <p style="margin:.75em;">보호자를 동반하지 않고 혼자 여행하는 어린 승객에게는
+                        제한 사항이 적용될 수 있습니다.</p>
+                      </li>
+                    </ul>
+                  </div>
           </div>
+          <div class="recommendProducts">
+            가이드 상품 추천 영역
+          </div>
+
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
