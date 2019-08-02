92a7d1b01f4127c7d8bd5963b079a31ee64574b2
류정관 <starlight747@naver.com>
2019-08-02 18:00:58 +0900

    류정관|Req 3-x| DB controller index swagger 가이드서비스 리뷰 수정

--- backend/routes/api/review/index.js
+++ backend/routes/api/review/index.js
@@ -2,8 +2,7 @@ const router=require('express').Router()
 const controller=require('./controller')
 
 router.post('/create/:id',controller.createReview)
-router.put('/update/:id/:title/:content',controller.updateReview)
-router.delete('/delete/:id/:title/:content',controller.deleteReview)
-
+router.put('/update/:id',controller.updateReview)
+router.delete('/delete/:id',controller.deleteReview)
 
 module.exports = router
