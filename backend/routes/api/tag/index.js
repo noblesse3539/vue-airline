ffd1741bde32928949bbeb7378796018d255bdbd
opalcat <noblesse3539@daum.net>
2019-08-08 19:45:25 +0900

    opalcat| 가이드체크 미들웨어 작성, 태그에 미들웨어 추가

--- backend/routes/api/tag/index.js
+++ backend/routes/api/tag/index.js
@@ -1,9 +1,13 @@
 const router=require('express').Router()
 const controller=require('./controller')
+const authMiddleware = require('../../../middlewares/auth')
+const guideCheck = require('../../../middlewares/guideCheck')
 
 router.get('/findGSByTag/:tag',controller.findGSByTag)
-router.post('/create/:id',controller.createTag)
-router.delete('/delete/:id/:tag',controller.deleteTag)
-router.put('/update/:id/:tag',controller.updateTag)
+router.post('/create/:id', authMiddleware, guideCheck, controller.createTag)
+router.delete('/delete/:id/:tag', authMiddleware, guideCheck, controller.deleteTag)
+router.put('/update/:id/:tag', authMiddleware, guideCheck, controller.updateTag)
 router.get('/', controller.getTagList)
+
+
 module.exports = router
