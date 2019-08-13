c6186ef1f514ee9055246ff3541218c98ba41c3f
류정관 <starlight747@naver.com>
2019-08-13 18:02:38 +0900

    류정관|채팅방 오류 수정

--- bision_project/src/components/Room.vue
+++ bision_project/src/components/Room.vue
@@ -12,11 +12,9 @@
             </span><br />
             <span>
               {{one.user? one.user.email : ''}}
-            </span><br />
-            <span v-html="lastChatMsg(one._id)">
             </span>
           </div>
-          <div class="chatroom-latest-time" v-html="lastChatTime(one._id)">
+          <div class="chatroom-latest-time">
           </div>
         </div>
         <div @click="join(one._id)" v-else class="chatroom-box">
@@ -29,11 +27,7 @@
             </span><br />
             <span>
               {{one.guide? one.guide.email : ''}}
-            </span><br />
-            <span v-html="lastChatMsg(one._id)">
             </span>
-          </div>
-          <div class="chatroom-latest-time" v-html="lastChatTime(one._id)">
           </div>
         </div>
       </v-flex>
@@ -116,19 +110,13 @@ export default {
               console.log(err);
             });
         },
-        // getUser(msg){
-        //   if(msg.user===undefined){
-        //     return msg.guide.username
-        //   }else{
-        //     return msg.user.username
-        //   }
-        // },
         getRoomList(){
+          console.log('getIsGuide',this.getIsGuide)
           if(this.getIsGuide){
             this.$http.get('/api/chat/roomListByGuide/'+this.getuserId)
             .then(res=>{
               this.chatList=res.data;
-              console.log("what sithis", res)
+              console.log(this.chatList);
             })
             .then(res=>{
               this.getRoomListAdd()
@@ -137,81 +125,14 @@ export default {
             this.$http.get('/api/chat/roomListByUser/'+this.getuserId)
             .then(res=>{
               this.chatList=res.data;
+              console.log(this.chatList);
             })
             .then(res=>{
               this.getRoomListAdd()
             })
           }
         },
-        lastChatMsg(lcMsg){
-          this.$http.get('/api/chat/lastChatMsg/'+lcMsg)
-          .then(res=>{
-            console.log('lcMsg',res.data);
-            return res.data
-          })
-        },
-        lastChatTime(lcTime){
-          this.$http.get('/api/chat/lastChatTime/'+lcTime)
-          .then(res=>{
-            return res.data
-          })
-        },
-        // getLocalTimezone(msg){
-        //   let year=msg.created_at.substring(0,4)
-        //   let month=msg.created_at.substring(5,7)
-        //   let day=msg.created_at.substring(8,10)
-        //   let hour=msg.created_at.substring(11,13)
-        //   let min=msg.created_at.substring(14,16)
-        //   let sec=msg.created_at.substring(17,19)
-        //   let ms=msg.created_at.substring(20,23)
-        //   // console.log(year+'년 '+month+'월 '+day+'일 '+hour+'시 '+min+'분 '+sec+'초 '+ms+'밀리초');
-        //   let newDate=new Date(year,month-1,day,hour,min,sec,ms)
-        //   // return new Date(msg.created_at.getTime()-(newDate.getTimezoneOffset()*60*1000))
-        //   let newDate2=new Date(newDate.getTime()-(newDate.getTimezoneOffset()*60*1000))
-        //   let Time
-        //   Time=(newDate2.getYear()+1900).toString()+'년 '
-        //       +(newDate2.getMonth()+1).toString()+'월 '
-        //       +newDate2.getDate().toString()+'일 '
-        //       +newDate2.toString().substring(16,18)+':'
-        //       +newDate2.toString().substring(19,21)
-        //
-        //   // 월의 값이 한 자리 수 이면 0을 추가
-        //   if(Time.substring(6,9).indexOf(' ')!==-1){
-        //     Time=Time.substring(0,6)+'0'+Time.substring(6,Time.length)
-        //   }
-        //
-        //   // 일의 값이 한 자리 수 이면 0을 추가
-        //   if(Time.substring(10,13).indexOf(' ')!==-1){
-        //     Time=Time.substring(0,10)+'0'+Time.substring(10,Time.length)
-        //   }
-        //
-        //   // 오전 오후 구분
-        //   if(Time.substring(14,16)>"12"){ // 시를 오후 1시로
-        //     Time=Time.substring(0,14)+'오후 '+(Time.substring(14,16)-12)+Time.substring(16,Time.length)
-        //   }else if(Time.substring(14,16)=="12"){ // 12시를 오후 12시로
-        //     Time=Time.substring(0,14)+'오후 '+Time.substring(14,16)+Time.substring(16,Time.length)
-        //   }else if(Time.substring(14,16)=="00"){ // 00시를 오전 12시로
-        //     Time=Time.substring(0,14)+'오전 '+12+Time.substring(16,Time.length)
-        //   }else{ // 01시를 오전 01시로
-        //     Time=Time.substring(0,14)+'오전 '+Time.substring(14,Time.length)
-        //   }
-        //
-        //
-        //   // 당일 메시지는 날짜 표시 안하고 시간만 표시.
-        //   // 당일이 아니면 날짜만 표시
-        //   let today=new Date();
-        //   if(today.getYear() === newDate2.getYear() &&
-        //     today.getMonth() === newDate2.getMonth()&&
-        //     today.getDate()=== newDate2.getDate()){
-        //       Time=Time.substring(13,Time.length)
-        //   }else{
-        //     Time=Time.substring(0,13)
-        //   }
-        //
-        //   return Time
-        // },
         getRoomListAdd(){
-          
           let url=''
           if(this.getIsGuide){
             url='/api/room/findByGuide/'
